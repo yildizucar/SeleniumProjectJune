65da0442c5450e07b6a9c0a5c614d91cc1ad0a22
yildiz ucar <yildizucr@gmail.com>
2021-07-01 04:30:27 -0400

    Added second updated

--- src/test/java/HelloTestJune.java
+++ src/test/java/HelloTestJune.java
@@ -1,6 +1,8 @@
 public class HelloTestJune {
     public static void main(String[] args) {
         System.out.println("Hello Test");
+        System.out.println("About to share project on Github");
+
     }
 
 }
